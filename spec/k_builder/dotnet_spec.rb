62e22aa4746a6187629e7b6f489452a61d12cbda
klueless-io <david@ideasmen.com.au>
2021-03-29 11:02:42 +1100

    first commit

---
+++ spec/k_builder/dotnet_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe KBuilder::Dotnet do
+  it "has a version number" do
+    expect(KBuilder::Dotnet::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
